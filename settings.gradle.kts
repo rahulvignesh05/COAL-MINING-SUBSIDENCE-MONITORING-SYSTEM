06b99397c2a4dd4a6aa16c8952fd29f475217c6d
rahulvignesh05 <rahulvignesh05@gmail.com>
2026-09-23 14:22:14 +0530

    Add Gradle settings for project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") } // needed for osmdroid
+    }
+}
+
+rootProject.name = "SubsidenceWorkerApp"
+include(":app")
